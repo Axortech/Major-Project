f1fee20e8f16670c00ea7e12af7b0af263b35037
Axortech <axortechnp@gmail.com>
2025-02-06 20:24:17 +0545

    Backend File Removed

--- backend/common/utility.py
+++
@@ -1,4 +0,0 @@
-import random
-def generate_discount_code():
-
-    return str(random.randint(100, 999))
--- backend/product/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import ProductSearchOrScrapeView
-
-urlpatterns = [
-    path('api/product/', ProductSearchOrScrapeView.as_view(), name='product-search-or-scrape'),
-]
